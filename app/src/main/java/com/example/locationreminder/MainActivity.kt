f9c74b8c9226a6cc2792f94f2158dec23f7cd0b6
Tanvir <rahman.tanvir.ovi@gmail.com>
2022-08-16 00:28:21 +0200

    Added DataBinding, Material Toolbar and some needed dependencies

---
+++ app/src/main/java/com/example/locationreminder/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.locationreminder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import com.example.locationreminder.ui.main.MainFragment
+import com.google.android.material.internal.ToolbarUtils
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
---
+++ app/src/main/java/com/example/locationreminder/ui/main/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.example.locationreminder.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.locationreminder.R
+
+class MainFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
